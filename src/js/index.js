4d616281f4f1386e33d2b70ebb1985bf97cf5288
Elena <elena.do1988@yandex.ru>
2019-12-28 18:06:31 +0300

    fonts, imgs are added

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,6 @@
 import "./import/modules";
 import "./import/components";
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
                         ]
                     }
                 }
+            },
+            {
+                test: /\.svg$/,
+                loader: 'gulp-svg-sprite'
             }
         ]
     },
